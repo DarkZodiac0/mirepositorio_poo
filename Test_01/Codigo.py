830b5d1d4e04bf9e172d1bd67b1206e1920f377d
DarkZodiac0 <axel0030105@hotmail.com>
2026-08-24 12:03:57 -0600

    Implement user data input in Codigo.py
    Added input prompts for user data collection.

---
+++ Test_01/Codigo.py
@@ -0,0 +1,12 @@
+# Codigo hecho por Axel Arturo Pérez Montero
+
+#Datos 
+
+nombre = input("Dame tu nombre");
+apellido = input("dame tu apellido")
+numerocta = input("dame tu numero de cuenta para ayudarte")
+
+#Datos recibidos
+
+print("\n---Datos registrados---")
+print("Usted ha sido roabdo")
